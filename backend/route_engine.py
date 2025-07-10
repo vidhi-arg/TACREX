5a9e465028f6fe7e2814200a3d16ca0498ca27e3
vidhi-arg <abstractartist7468@gmail.com>
2025-07-10 16:44:28 +0530

    Update route_engine.py

--- backend/route_engine.py
+++ backend/route_engine.py
@@ -1,1 +1,17 @@
+import openrouteservice
+from openrouteservice import convert
+
+client = openrouteservice.Client(key="eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6ImQ5YjExY2JkMzI1NDRmMDlhYjU3OTc4NWZjOGY1MzBmIiwiaCI6Im11cm11cjY0In0=")  # Replace this
+
+def get_route(start_coords, end_coords):
+    try:
+        route = client.directions(
+            coordinates=[start_coords, end_coords],
+            profile='foot-walking',  # can also use 'driving-car'
+            format='geojson'
+        )
+        return route
+    except Exception as e:
+        print("Route error:", e)
+        return None
 
