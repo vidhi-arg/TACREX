8267053c6f40a86fb1b2b387f8759f5956e2a2e7
vidhi-arg <abstractartist7468@gmail.com>
2025-07-10 18:13:46 +0530

    Update risk_evaluator.py

--- backend/risk_evaluator.py
+++ backend/risk_evaluator.py
@@ -1,20 +1,17 @@
 from geopy.distance import geodesic
 
 def evaluate_route_risk(route_coords, threat_zones):
-    danger_hits = 0
-
+    hits = []
     for point in route_coords:
         for threat in threat_zones:
-            threat_center = (threat["lat"], threat["lon"])
-            distance = geodesic(point, threat_center).meters
+            dist = geodesic(point, [threat["lat"], threat["lon"]]).meters
+            if dist < threat["radius"]:
+                hits.append(threat)
 
-            if distance < threat["radius"]:
-                danger_hits += 1
-
-    if danger_hits == 0:
-        return " Safe"
-    elif danger_hits <= 3:
-        return " Borderline"
+    if len(hits) == 0:
+        return "Safe", []
+    elif len(hits) <= 2:
+        return "Borderline", hits
     else:
-        return " Unsafe"
+        return "High Risk", hits
 
